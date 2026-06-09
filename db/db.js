5a683e8dcd065c5700a5c26a26d7e5ced59d12ba
MayankParmar1005 <mayankparmar2007@gmail.com>
2026-06-09 16:56:19 +0530

    db.js file changes.

--- db/db.js
+++ db/db.js
@@ -1,11 +1,23 @@
+// const { Pool } = require('pg');
+
+// const pool = new Pool({
+//   user: 'postgres',
+//   host: '192.168.0.166',
+//   database: 'Employee',
+//   password: 'shree',
+//   port: 5432
+// });
+
+// module.exports = pool;
+
 const { Pool } = require('pg');
 
+// Railway automatically provides process.env.DATABASE_URL if you added it to the Variables tab
 const pool = new Pool({
-  user: 'postgres',
-  host: '192.168.0.62',
-  database: 'Employee',
-  password: 'shree',
-  port: 5432
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: true // Neon requires secure SSL connections
+  }
 });
 
 module.exports = pool;
